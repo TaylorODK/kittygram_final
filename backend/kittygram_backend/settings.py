e46e557894841f95e67292bfd8a5ca358151d5a7
Mikhail Bashirov <bashirovma@gmail.com>
2024-12-14 20:48:09 +0700

    test

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -56,12 +56,12 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5432)
+        'PORT': os.getenv('DB_PORT', 5431)
     }
 }
 
